59e0d9093377b4455d081323c075ef3ec983cfed
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-07-17 19:35:37 +0900

    Add simulation output for adaptive DMA testbench
    Added output log for adaptive DMA testbench simulation, detailing burst triggers and analysis results.

---
+++ Practice Questions/04.sv/Output.sv
@@ -0,0 +1,83 @@
+/*
+
+# ---- INITIALIZING ADAPTIVE DMA TESTBENCH ---
+[GEN @ 0 ns] Burst Triggered | Addr: 32'h a6d221f5 | Length: 10 Beats
+[GEN @ 0 ns] Burst Triggered | Addr: 32'h 5a3b484f | Length:  7 Beats
+[GEN @ 0 ns] Burst Triggered | Addr: 32'h 2b872258 | Length:  4 Beats
+[GEN @ 0 ns] Burst Triggered | Addr: 32'h a30e75da | Length: 10 Beats
+[GEN @ 105 ns] Burst Triggered | Addr: 32'h caa27493 | Length:  8 Beats
+[SCB @ 106 ns] -------------- Analyzing Burst (Addr: 32'ha6d221f5 --------------
+[SCB @ 106 ns] PASS | Lengths MATCH | Expected 10 BEATS, Captured: 10
+[SCB @ 106 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 185 ns] Burst Triggered | Addr: 32'h b6316be9 | Length: 15 Beats
+[SCB @ 186 ns] -------------- Analyzing Burst (Addr: 32'h5a3b484f --------------
+[SCB @ 186 ns] PASS | Lengths MATCH | Expected 7 BEATS, Captured: 7
+[SCB @ 186 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 235 ns] Burst Triggered | Addr: 32'h b6905a7e | Length:  4 Beats
+[SCB @ 236 ns] -------------- Analyzing Burst (Addr: 32'h2b872258 --------------
+[SCB @ 236 ns] PASS | Lengths MATCH | Expected 4 BEATS, Captured: 4
+[SCB @ 236 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 345 ns] Burst Triggered | Addr: 32'h a658ac1b | Length:  6 Beats
+[SCB @ 346 ns] -------------- Analyzing Burst (Addr: 32'ha30e75da --------------
+[SCB @ 346 ns] PASS | Lengths MATCH | Expected 10 BEATS, Captured: 10
+[SCB @ 346 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 435 ns] Burst Triggered | Addr: 32'h d8d64d44 | Length:  4 Beats
+[SCB @ 436 ns] -------------- Analyzing Burst (Addr: 32'hcaa27493 --------------
+[SCB @ 436 ns] PASS | Lengths MATCH | Expected 8 BEATS, Captured: 8
+[SCB @ 436 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 595 ns] Burst Triggered | Addr: 32'h da490226 | Length: 13 Beats
+[SCB @ 596 ns] -------------- Analyzing Burst (Addr: 32'hb6316be9 --------------
+[SCB @ 596 ns] PASS | Lengths MATCH | Expected 15 BEATS, Captured: 15
+[SCB @ 596 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 645 ns] Burst Triggered | Addr: 32'h c220cba6 | Length:  5 Beats
+[SCB @ 646 ns] -------------- Analyzing Burst (Addr: 32'hb6905a7e --------------
+[SCB @ 646 ns] PASS | Lengths MATCH | Expected 4 BEATS, Captured: 4
+[SCB @ 646 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 715 ns] Burst Triggered | Addr: 32'h e8c5a6f7 | Length: 10 Beats
+[SCB @ 716 ns] -------------- Analyzing Burst (Addr: 32'ha658ac1b --------------
+[SCB @ 716 ns] PASS | Lengths MATCH | Expected 6 BEATS, Captured: 6
+[SCB @ 716 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 765 ns] Burst Triggered | Addr: 32'h e7969dc1 | Length:  5 Beats
+[SCB @ 766 ns] -------------- Analyzing Burst (Addr: 32'hd8d64d44 --------------
+[SCB @ 766 ns] PASS | Lengths MATCH | Expected 4 BEATS, Captured: 4
+[SCB @ 766 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 905 ns] Burst Triggered | Addr: 32'h 8f22c16f | Length: 14 Beats
+[SCB @ 906 ns] -------------- Analyzing Burst (Addr: 32'hda490226 --------------
+[SCB @ 906 ns] PASS | Lengths MATCH | Expected 13 BEATS, Captured: 13
+[SCB @ 906 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[GEN @ 965 ns] Burst Triggered | Addr: 32'h b46c8d9d | Length: 15 Beats
+[SCB @ 966 ns] -------------- Analyzing Burst (Addr: 32'hc220cba6 --------------
+[SCB @ 966 ns] PASS | Lengths MATCH | Expected 5 BEATS, Captured: 5
+[SCB @ 966 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[SCB @ 1076 ns] -------------- Analyzing Burst (Addr: 32'he8c5a6f7 --------------
+[SCB @ 1076 ns] PASS | Lengths MATCH | Expected 10 BEATS, Captured: 10
+[SCB @ 1076 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[SCB @ 1136 ns] -------------- Analyzing Burst (Addr: 32'he7969dc1 --------------
+[SCB @ 1136 ns] PASS | Lengths MATCH | Expected 5 BEATS, Captured: 5
+[SCB @ 1136 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[SCB @ 1286 ns] -------------- Analyzing Burst (Addr: 32'h8f22c16f --------------
+[SCB @ 1286 ns] PASS | Lengths MATCH | Expected 14 BEATS, Captured: 14
+[SCB @ 1286 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+[SCB @ 1446 ns] -------------- Analyzing Burst (Addr: 32'hb46c8d9d --------------
+[SCB @ 1446 ns] PASS | Lengths MATCH | Expected 15 BEATS, Captured: 15
+[SCB @ 1446 ns] PASS | ALL PAYLOAD DATATYPES MATCHED SUCCESSFULLY!
+
+# --- SIMULATION COMPLETE ---
+
+
+
+*/
